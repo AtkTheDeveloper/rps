7074d09605dfd6d8b443d6c488b08c18b50666e5
Atk The Developer <182418129+AtkTheDeveloper@users.noreply.github.com>
2025-11-10 11:08:50 +0000

    Created basic functionalities

---
+++ script.js
@@ -0,0 +1,63 @@
+function getComputerChoice(){
+    let random = Math.floor(Math.random() * 3);
+    switch (random) {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+        default:
+            break;
+    }
+}
+
+function getHumanChoice(){
+    let user_input = prompt("Enter Your Choice: ");
+    return user_input.toLowerCase();
+}
+
+
+
+
+function playGame(){
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice){
+
+    console.log(`HumanChoice: ${humanChoice}`);
+    console.log(`ComputerChoice: ${computerChoice}`);
+
+    if(humanChoice === computerChoice){
+        console.log("We have a tie");
+        return;
+    }
+
+    if(humanChoice === "rock" && computerChoice === "scissors" ||
+       humanChoice === "paper" && computerChoice === "rock" ||
+       humanChoice === "scissors" && computerChoice === "paper"
+    ){
+        console.log("HUMAN WINS");
+        humanScore = humanScore + 1;
+        console.log(humanScore);
+    } else{
+        console.log("COMPUTER WINS");
+        computerScore = computerScore + 1;
+        console.log(computerScore);
+    }
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+}
+
+for(let i = 0; i < 5; i++){
+    playGame()
+}
